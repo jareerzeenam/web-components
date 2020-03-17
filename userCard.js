a8d667e584b9633b6e08499a89b41dcdcafb961a
imjareer <jareer@gmail.com>
2020-03-18 01:14:44 +0530

    slot added for email and phone

--- userCard.js
+++ userCard.js
@@ -1,19 +1,40 @@
 const template = document.createElement("template");
 template.innerHTML = `
 <style>
-h3{
-    color:coral;
-}
+.user-card {
+		font-family: 'Arial', sans-serif;
+		background: #f4f4f4;
+		width: 500px;
+		display: grid;
+		grid-template-columns: 1fr 2fr;
+		grid-gap: 10px;
+		margin-bottom: 15px;
+		border-bottom: darkorchid 5px solid;
+	}
+
+	.user-card img {
+		width: 100%;
+	}
+
+	.user-card button {
+		cursor: pointer;
+		background: darkorchid;
+		color: #fff;
+		border: 0;
+		border-radius: 5px;
+		padding: 5px 10px;
+	}
 </style>
+
 <div class="user-card">
     <img />    
     <div>
         <h3></h3>
         <div class="info">
-            <p>Email</p>
-            <p>Phone</p>
+            <p><slot name="email"/></p>
+            <p><slot name="phone"/></p>
         </div>
-        <button id="toggle-info"></button>
+        <button id="toggle-info">Hide Info</button>
     </div>
 </div>
 `;
